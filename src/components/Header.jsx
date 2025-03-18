e03723307919458b7702bbb8e83fb012bf1ff964
TizzleMeDis <54555309+TizzleMeDis@users.noreply.github.com>
2025-03-18 04:10:19 -0700

    Completed a transitional help screen that explain the rules

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ import React, {useState} from 'react'
 import {HeaderLogo} from './header/index'
 
 export default function Header({setFinished, setPlay, setCurrentQ, setAnswers}) {
+  const [visible, setVisible] = useState(false)
 
   const handleRefresh = () => {
     setPlay(false);
@@ -18,7 +19,7 @@ export default function Header({setFinished, setPlay, setCurrentQ, setAnswers}) 
   }
   
   const handleHelpScreen = () => {
-
+    setVisible(!visible)
   }
 
   return (
@@ -27,8 +28,18 @@ export default function Header({setFinished, setPlay, setCurrentQ, setAnswers}) 
         <li className='link'><a href='./'><HeaderLogo /></a></li>
         <li className='link'><button onClick={handleAutoComplete}>Finish</button></li>
         <li className='link'><button onClick={handleRefresh}>Restart</button></li>
-        <li className='link'><button>Help</button></li>
+        <li className='link'><button onClick={handleHelpScreen}>Help</button></li>
       </ul>
+
+      <div className={`help-screen${visible ? " invisible" : ""}`}>
+        <h1 className='help-screen-title'>Help Screen</h1>
+        <ul className='help-screen-list'>
+          <li><p className='help-screen-text'>Want to reset the settings just press the logo Open-Trivia</p></li>
+          <li><p className='help-screen-text'>If you want to restart the quiz just hit restart to jump you to the beginning</p></li>
+          <li><p className='help-screen-text'>Lastly if you're done just click the Finish button to complete the quiz and see how many you got.</p></li>
+          <li><p className='help-screen-text'>If you're done with the help screen just click help.</p></li>
+        </ul>
+      </div>
     </div>
   )
 }
